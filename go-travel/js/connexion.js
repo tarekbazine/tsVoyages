be9aeb8843cbae539d4567980921408071f522ca
ThinhinaneIHADADENE <et_ihadadene@esi.dz>
2018-05-16 21:49:17 +0100

    focus events

--- go-travel/js/connexion.js
+++ go-travel/js/connexion.js
@@ -1,12 +1,18 @@
 $(document).ready(function () {
     $("#erreur").hide()
 
+    $("#email_connexion").focus(function(){
+        $("#erreur").hide();
+    });
+    $("#mot_de_passe_connexion").focus(function () {
+        $("#erreur").hide();
+    });
     $("#connexion_form").submit(function (e) {
         e.preventDefault();
         var email=$("#email_connexion").val();
         var mdp = $("#mot_de_passe_connexion").val();
 
-        if(email=='tina' && mdp=='tina')  window.location.replace("file:///E:/IHMPrototype/go-travel/index.html");
+        if(email=='tina@esi.dz' && mdp=='tina')  window.location.replace("file:///E:/IHMPrototype/go-travel/index.html");
         else {$("#erreur").text('Utilisateur ou mot de passe incorrect');
         $("#erreur").show();
         }
